bdeaa78c9f54ced8cb31bd9db9581cd5beb4a4f2
dewintanur <dewintaistiqomah@gmail.com>
2025-08-31 21:30:42 +0700

    First commit - add portfolio navbar project

---
+++ project-data.js
@@ -0,0 +1,50 @@
+const projects = {
+  1: {
+    title_id: "Halaman Admin Prestasi Mahasiswa",
+    title_en: "Student Achievement Admin Page",
+    images: ["asset/presmaa.png", "asset/regis_prsma.png", "asset/pbl1.JPEG", "asset/pbl2.JPEG"],
+    desc_id: "Sebuah aplikasi berbasis web untuk mengelola data prestasi mahasiswa, dengan fitur CRUD, autentikasi, dan dashboard interaktif. Projek ini dilakukan secara berkelompok, dimana saya berperan sebagai Backend Developer. Dalam projek ini saya menggunakan Laravel sebagai framework backend dan Bootstrap untuk tampilan frontend. Dalam projek ini saya juga menggunakan beberapa package seperti Spatie untuk manajemen role dan permission, Livewire untuk interaksi frontend, dan Tailwind CSS untuk styling. Saya juga menggunakan MySQL sebagai database untuk menyimpan data. Pada projek ini saya memiliki pengalaman dalam mengerjakan projek secara berkelompok, dimana saya belajar untuk berkomunikasi dan berkolaborasi dengan anggota tim lainnya.",
+    desc_en: "A web-based application to manage student achievement data, with CRUD features, authentication, and an interactive dashboard. This project was done in a team, where I worked as a Backend Developer. I used Laravel as the backend framework and Bootstrap for the frontend. I also implemented several packages such as Spatie for role and permission management, Livewire for frontend interaction, and Tailwind CSS for styling. MySQL was used as the database. Through this project, I gained experience working in a team, learning how to communicate and collaborate effectively with other members.",
+    repo: "",
+    demo: "https://youtu.be/Fx5jW0lX4IU?si=Z6yXV-wCZg0goIrt",
+    video: "https://www.youtube.com/embed/Fx5jW0lX4IU?si=YKJdQLPjwZe58f6x",
+     languages: {
+      JavaScript: 40,
+      PHP: 35,
+      Blade: 20,
+      CSS: 5
+    }
+  },
+  2: {
+    title_id: "Website Reservify",
+    title_en: "Reservify Website",
+    images: ["asset/login.png", "asset/register.png", "asset/profile.png",  "asset/pemesanan.png", "asset/invoice.png", "asset/dokum.png"],
+    desc_id: "Website pemesanan berbasis Laravel dan Bootstrap. Website ini memungkinkan pengunjung untuk reservasi meja di restoran dengan mudah. Pengunjung dapat melakukan registrasi, login, dan mengelola profil mereka. Selain itu, pengunjung juga dapat melakukan reservasi meja, melihat daftar reservasi yang telah dibuat, dan mengunduh invoice sebagai bukti reservasi. Website ini merupakan projek individu yang saya kerjakan pada saat semester 4, dimana saya belajar banyak tentang cara membuat website yang user-friendly dan cara mengelola data pengunjung. Saya juga belajar banyak tentang cara membuat website yang responsif dan dapat diakses dari berbagai perangkat.",
+    desc_en: "A reservation website built with Laravel and Bootstrap. This website allows visitors to easily book tables at restaurants. Visitors can register, log in, and manage their profiles. They can also make reservations, view their reservation history, and download invoices as proof of booking. This was an individual project in my 4th semester, where I learned a lot about building user-friendly websites, managing visitor data, and creating responsive designs accessible from multiple devices.",
+    repo: "https://github.com/dewintanur/reservfiy",
+    demo: "https://youtu.be/_chTpyseveA?si=34lcLKXE4-3PhY37",
+    video: "https://www.youtube.com/embed/_chTpyseveA?si=l1XDmCEv6bUcARD_",
+     languages: {
+      JavaScript: 75,
+      PHP: 35,
+      Blade: 20,
+      CSS: 5
+    }
+  },
+  3: {
+    title_id: "Website Check-in Check-out Kehadiran Pengunjung",
+    title_en: "Visitor Check-in/Check-out Website",
+    images: ["asset/form.png", "asset/isidata.png", "asset/bookinglist.png"],
+    desc_id: "Website kehadiran pengunjung berbasis Laravel dan Bootstrap. Memungkinkan pengunjung untuk check-in dan check-out dengan mudah. Website ini juga memiliki fitur untuk mengelola data pengunjung, seperti melihat daftar pengunjung yang telah check-in dan check-out, serta mengelola data booking ruangan. Website ini awalnya merupakan projek kelompok pada saat magang di semester 5, namun saya kembangkan menjadi tugas akhir. Dalam projek ini saya mengalami banyak kendala, seperti kesulitan dalam mengelola data pengunjung, namun saya berhasil menyelesaikan projek ini dengan baik.",
+    desc_en: "A visitor attendance website built with Laravel and Bootstrap. It allows visitors to easily check in and check out. The website also includes features for managing visitor data, such as viewing lists of checked-in and checked-out visitors, as well as managing room bookings. This project started as a group project during my internship in semester 5, but I later developed it into my final project. During this project, I faced many challenges, such as difficulties in managing visitor data, but I successfully completed it and learned a lot about building user-friendly systems.",
+    repo: "https://github.com/dewintanur/emcico",
+    demo: "https://youtu.be/DIITTD4R1WY",
+    video: "https://www.youtube.com/embed/DIITTD4R1WY?si=FxzOqdEVq-Xt_b1p",
+     languages: {
+      JavaScript: 40,
+      PHP: 50,
+      Blade: 50,
+      CSS: 5
+    }
+  }
+};
